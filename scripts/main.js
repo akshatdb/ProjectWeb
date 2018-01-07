571e86c5bb516c2bd5ed7c9500373af34d722f1c
Akshat Dubey <akshatd4@gmail.com>
2018-01-07 13:14:40 +0530

    Changed looks

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,13 @@ $('#file').on('change',function(){
 		var tmp = $('#file').prop('files')[0];
 		$('#file-upload-label').attr('for','submit-btn');
 		$('#file-upload-label').html('Upload');
+		$('.form').css({
+			'top':'40%'
+		})
 		$('#image-view').attr('src',URL.createObjectURL(tmp));
+		$('body').css({
+			'background-image':'url(images/backblur.png)'
+		});
 		$('#image-view').fadeIn();
 	}
 });
